b49bdba3d3363c07f1c14a69bbabc2a4271ee10b
Denis Kalyakin <denis.kalyakin999@gmail.com>
2024-09-25 23:39:23 +0300

    train conflict to

--- src/main/java/com/example/artvswar/ArtVsWarApplication.java
+++ src/main/java/com/example/artvswar/ArtVsWarApplication.java
@@ -1,13 +1,13 @@
 package com.example.artvswar;
 
+import java.util.Arrays;
 import com.example.artvswar.service.EmailExternalService;
+import com.example.artvswar.service.impl.EmailExternalServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
@@ -15,11 +15,12 @@ import java.util.Arrays;
 public class ArtVsWarApplication {
 
     public static void main(String[] args) {
-
         ConfigurableApplicationContext context = SpringApplication.run(ArtVsWarApplication.class, args);
+//        Arrays.stream(context.getBe).forEach(System.out::println);
+//        Arrays.stream(context.getBeanDefinitionNames())
+//                .map(name -> context.getBean(name).getClass())
+//                .forEach(System.out::println);
+//        System.out.println(context.getBean(EmailExternalServiceImpl.class));
         System.out.println(context.getBean(EmailExternalService.class));
-        Arrays.stream(context.getBeanDefinitionNames())
-                .map(name -> context.getBean(name).getClass())
-                .forEach(System.out::println);
     }
 }
