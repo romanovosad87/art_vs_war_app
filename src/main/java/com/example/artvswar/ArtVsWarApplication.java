0ada8c5392913df1963bc673f805eede94a81a12
roman <roman.novosad@yahoo.com>
2024-09-25 23:37:06 +0300

    train conflict resolution

--- src/main/java/com/example/artvswar/ArtVsWarApplication.java
+++ src/main/java/com/example/artvswar/ArtVsWarApplication.java
@@ -1,9 +1,14 @@
 package com.example.artvswar;
 
+import com.example.artvswar.service.EmailExternalService;
+import com.example.artvswar.service.impl.EmailExternalServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,6 +16,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ArtVsWarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArtVsWarApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(ArtVsWarApplication.class, args);
+        System.out.println(context.getBean(EmailExternalService.class));
+        Arrays.stream(context.getBeanDefinitionNames())
+                .map(name -> context.getBean(name).getClass())
+                .forEach(System.out::println);
     }
 }
