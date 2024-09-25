339c055f67fd07a7d612c17f10466242c66da6aa
roman <roman.novosad@yahoo.com>
2024-09-25 23:37:22 +0300

    train conflict resolution

--- src/main/java/com/example/artvswar/ArtVsWarApplication.java
+++ src/main/java/com/example/artvswar/ArtVsWarApplication.java
@@ -1,7 +1,6 @@
 package com.example.artvswar;
 
 import com.example.artvswar.service.EmailExternalService;
-import com.example.artvswar.service.impl.EmailExternalServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
