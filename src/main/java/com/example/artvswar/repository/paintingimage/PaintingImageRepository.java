249be18c11e525717048df49e566ce1b502ddd33
Denis Kalyakin <denis.kalyakin999@gmail.com>
2024-10-14 23:27:05 +0300

    PaintingImageRepository was deleted

--- src/main/java/com/example/artvswar/repository/paintingimage/PaintingImageRepository.java
+++ src/main/java/com/example/artvswar/repository/paintingimage/PaintingImageRepository.java
@@ -3,6 +3,5 @@ package com.example.artvswar.repository.paintingimage;
 import com.example.artvswar.model.PaintingImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaintingImageRepository extends JpaRepository<PaintingImage, Long>,
-        CustomPaintingImageRepository {
+public interface PaintingImageRepository extends JpaRepository<PaintingImage, Long>{
 }
